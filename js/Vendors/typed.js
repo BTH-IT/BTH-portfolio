331e44291230c2c233d33524a5bda288eb87a17e
Bien Thanh Hung <62289622+BTH-IT@users.noreply.github.com>
2024-05-04 15:18:41 +0700

    fix bugs

--- js/Vendors/typed.js
+++ js/Vendors/typed.js
@@ -4,18 +4,4 @@ const typed = new Typed(".auto-typed", {
   backSpeed: 100,
   loop: true,
   smartBackspace: true,
-  onComplete: function(self) {
-    if (self.strings[self.sequenceIndex] === "Bien Thanh Hung") {
-      self.el.innerHTML = '<span style="color: blue;">' + self.strings[self.sequenceIndex] + '</span>';
-    } else if (self.strings[self.sequenceIndex] === "Frontend Developer") {
-      self.el.innerHTML = '<span style="color: red;">' + self.strings[self.sequenceIndex] + '</span>';
-    }
-  },
-  onStringTyped: function(index, self) {
-    if (self.strings[index] === "Bien Thanh Hung") {
-      self.el.innerHTML = '<span style="color: blue;">' + self.strings[index] + '</span>';
-    } else if (self.strings[index] === "Frontend Developer") {
-      self.el.innerHTML = '<span style="color: red;">' + self.strings[index] + '</span>';
-    }
-  }
 });
